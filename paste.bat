279e4d3825859b13e05d151f29dc8f20662b6514
AndraTaylor <96328518+AndraTaylor@users.noreply.github.com>
2021-12-20 07:25:41 -0800

    Delete paste.bat

--- paste.bat
+++
@@ -1,4 +0,0 @@
-powershell -command $wsh = New-Object -ComObject Wscript.Shell 
-powershell -command $wsh.AppActivate("C:\Windows\system32\cmd.exe")
-powershell -command Start-Sleep 2 
-powershell -command $wsh.Sendkeys("{TAB}")
